c1a62c97be5fabf627a0f2fef308e1b0f6e09873
ptrbates <peterbates@protonmail.com>
2019-03-12 17:07:37 -0700

    add grid size chooser

--- main.js
+++ main.js
@@ -11,16 +11,16 @@ const createRow = n => {
   const row = document.createElement('div');
   row.className = 'row-div';
   for (let i = 0; i < n; i++) {
-    row.appendChild(createCell());
+    row.appendChild(createCell(n));
   }
   return row;
 }
 
-const createCell = () => {
+const createCell = n => {
   const cell = document.createElement('div');
   cell.className = 'inner-div';
-  cell.style.width = '56px';
-  cell.style.height = '56px';
+  cell.style.width = String(560 / n) + 'px';
+  cell.style.height = cell.style.width;
   cell.style.backgroundColor = 'rgba(0,0,0,0)';
   cell.addEventListener('mouseover', e => darkenBG(e));
   return cell;
@@ -36,4 +36,49 @@ const darkenBG = e => {
   }
 }
 
+const createSelector = () => {
+  const selector = document.createElement('div');
+  selector.className = 'selector';
+  selector.appendChild(createClear());
+  selector.addEventListener('mouseover', e => replaceClearWithInput(e));
+  return selector;
+}
+
+const createClear = () => {
+  const clear = document.createElement('p');
+  clear.textContent = 'Clear';
+  clear.style.fontSize = '13px';
+  clear.style.color = 'rgba(0,0,0,0.2)';
+  return clear;
+}
+
+const createInput = () => {
+  const input = document.createElement('input');
+  input.placeholder = 'squares per side?';
+  input.style.color = 'rgba(0,0,0,0.4)';
+  input.style.width = '150px';
+  input.addEventListener('keydown', e => {
+    if (e.keyCode === 13) {
+      if (e.target.value <= 100) {
+        replaceGrid(e.target.value);
+      } else {
+        input.placeholder = 'Not so greedy please.';
+        e.target.value = '';
+      }
+    }
+  });
+  return input;
+}
+
+const replaceClearWithInput = e => {
+  const parent = e.target.parentElement;
+  parent.replaceChild(createInput(), e.target);
+}
+
+const replaceGrid = n => {
+  const current = document.getElementById('container');
+  document.getElementById('main').replaceChild(createGrid(n), current);
+}
+
+document.getElementById('main').appendChild(createSelector());
 document.getElementById('main').appendChild(createGrid(10));
